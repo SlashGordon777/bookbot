c4f0274beb7ef5b0fa0dab73e5a055ba9bb6bc7c
Gordon Teschner <gt@php-pixel.de>
2025-04-07 19:37:41 +0200

    update

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from stats import get_num_words
+import sys
 
 
 def sort_on(dict):
     return dict["count"]
 
 def main():
-    with open("books/frankenstein.txt") as f:
+
+    if len(sys.argv) != 2:
+        print('Usage: python3 main.py <path_to_book>')
+        return sys.exit(1)
+
+    with open(sys.argv[1]) as f:
         file_contents = f.read()
         lowered_file_content = file_contents.lower()
         lowered_file_content.split()
@@ -24,7 +30,7 @@ def main():
 
         words_count = get_num_words(file_contents)
 
-        print("--- Begin report of books/frankenstein.txt --")
+        print(f"--- Begin report of {sys.argv[1]} --")
         print(f"{words_count} words found in the document")
         print("\n")
         for i in range(0, len(unique_letters_count_list)):
